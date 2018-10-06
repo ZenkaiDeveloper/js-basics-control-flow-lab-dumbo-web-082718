c41ff19f8506519b22196d94b5169128b4f7b32b
Andy Wang <wang.andy32@gmail.com>
2018-10-06 17:29:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
-// Write your code in this file!
-//et(199)).to.equal('This one is on me!');
-//     });
 
-//     it('charges 30 dollars for a distance over 2000 feet', function () {
-//       expect(scuberGreetingForFeet(2001)).to.equal('I will gladly take your thirty bucks.');
-//     });
-
-//     it('does not allow rides over 2500 feet', function () {
-//       expect(scuberGreetingForFeet(2501)).to.equal('No can do.');
-//     });
-//   });
 function scuberGreetingForFeet(distance){
   if(distance>2500){
     "No can do.";
   }elsif(distance<=400);{
     "This one is on me!";
   }elsif(distance>=2000){
-    "I will gladly take your thirty bucks."
+    "I will gladly take your thirty bucks.";
   }
 }
