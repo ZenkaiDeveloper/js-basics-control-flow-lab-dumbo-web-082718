83c6ffabbf85f7255a1d5f17d21cab66fc0868c1
Andy Wang <wang.andy32@gmail.com>
2018-10-06 17:25:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 // Write your code in this file!
+// //eet(199)).to.equal('This one is on me!');
+//     });
+
+//     it('charges 30 dollars for a distance over 2000 feet', function () {
+//       expect(scuberGreetingForFeet(2001)).to.equal('I will gladly take your thirty bucks.');
+//     });
+
+//     it('does not allow rides over 2500 feet', function () {
+//       expect(scuberGreetingForFeet(2501)).to.equal('No can do.');
+//     });
+//   });
 function scuberGreetingForFeet(distance){
   
 }
